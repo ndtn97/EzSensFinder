46ca50b5105146445f99c6849d06be77943ae220
ndtn97 <32271862+ndtn97@users.noreply.github.com>
2022-04-24 00:33:54 +0900

    license

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # EzSensFinder
+
+## License
+MIT
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 /*!
 * Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
 * Copyright 2013-2021 Start Bootstrap
+* Copyright 2022 ndtn97 (Modified)
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
 
 let minvals = [];
 let maxvals = [];
